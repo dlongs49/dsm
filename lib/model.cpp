e02e486b8ae660415577b0a5119d3e9c03043d6d
dinglong <dillonl49@163.com>
2024-03-02 01:37:21 +0800

    feat:#增加model

---
+++ lib/model.cpp
@@ -0,0 +1,6 @@
+#include "model.h"
+
+model::model(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ lib/model.h
@@ -0,0 +1,16 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <QWidget>
+
+class model : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit model(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // MODEL_H
