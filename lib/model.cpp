fc66601efa0c63a932d9fd80f467c88a9f042237
dinglong <dillonl49@163.com>
2024-03-02 19:32:54 +0800

    feat:#增加关闭操作

--- lib/model.cpp
+++ lib/model.cpp
@@ -15,6 +15,9 @@ Model::Model(QWidget *parent) : QWidget(parent) {
     this->setContentsMargins(1, 1, 1, 1);
 
     header_layout = new QHBoxLayout();
+    header_layout->setMargin(0);
+    header_layout->setSpacing(0);
+    header_layout->setDirection(QHBoxLayout::LeftToRight);
     header_widget = new QWidget(this);
     header_widget->setFixedWidth(width);
     header_widget->setFixedHeight(40);
@@ -22,13 +25,26 @@ Model::Model(QWidget *parent) : QWidget(parent) {
 
     header_title = new QLabel();
     header_title->setText("提示");
-    header_title->setStyleSheet("background:#ff0");
-    header_layout->addWidget(header_title);
+    header_title->setStyleSheet("margin-left:20px;font-size:14px");
+
+    close_btn = new QPushButton();
+    close_btn = new QPushButton();
+    close_btn->setIcon(QIcon(":/resource/close.png"));
+    close_btn->setIconSize(QSize(12, 12));
+    close_btn->setFixedSize(40, 20);
+    close_btn->setObjectName("close_btn");
+    close_btn->setCursor(Qt::PointingHandCursor);
+    connect(close_btn, SIGNAL(clicked()), this, SLOT(handleCloseBtn()));
+    close_btn->installEventFilter(this);
+
+    header_layout->addWidget(header_title, Qt::AlignLeft);
+    header_layout->addStretch(5);
+    header_layout->addWidget(close_btn, Qt::AlignRight);
     header_widget->setLayout(header_layout);
 //    header_layout->addWidget(header_widget);
 }
-void Model::paintEvent(QPaintEvent *event)
-{
+
+void Model::paintEvent(QPaintEvent *event) {
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
     painter.setBrush(QBrush("#ffffff"));
@@ -40,4 +56,21 @@ void Model::paintEvent(QPaintEvent *event)
 
     QWidget::paintEvent(event);
     painter.fillRect(this->rect().adjusted(4, 4, -4, -4), "#ffffff");
+}
+
+bool Model::eventFilter(QObject *obj, QEvent *event) {
+    QEvent::Type type = event->type();
+    if(type == QEvent::Enter){
+        this->close_btn->setIcon(QIcon(":/resource/close_hover.png"));
+        return true;
+    }
+    if(type == QEvent::Leave){
+        this->close_btn->setIcon(QIcon(":/resource/close.png"));
+        return true;
+    }
+    return QWidget::eventFilter(obj, event);
+}
+
+void Model::handleCloseBtn() {
+    this->hide();
 }
--- lib/model.h
+++ lib/model.h
@@ -1,5 +1,6 @@
 #ifndef MODEL_H
 #define MODEL_H
+
 #include <qpainter.h>
 #include <QWidget>
 #include <QtSvg/QSvgRenderer>
@@ -7,18 +8,35 @@
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 #include <QLabel>
+#include <QPushButton>
+#include <QPixmap>
+#include <QPainter>
+#include <QIcon>
+#include <QDebug>
+#include <QEvent>
 class Model : public QWidget {
 Q_OBJECT
 public:
     Model(QWidget *parent = nullptr);
+
     void paintEvent(QPaintEvent *event);
 
+    bool eventFilter(QObject *obj, QEvent *event);
+
+public slots:
+    void handleCloseBtn();
 private:
     QGraphicsDropShadowEffect *shadow;
     QHBoxLayout *header_layout;
     QWidget *header_widget;
     QLabel *header_title;
-    QSvgRenderer *render;
+    QSvgRenderer *renderer;
+    QPushButton *close_btn;
+    QPushButton *on_btn;
+    QPushButton *cancle_btn;
+    QPixmap *pixmap;
+    QPainter *painter;
+    QIcon *close_icon;
 signals:
 
 };
