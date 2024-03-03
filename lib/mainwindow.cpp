6f627779cd0e29ce0df936938d9c37fc5c2b20a8
dinglong <dillonl49@163.com>
2024-03-03 21:40:30 +0800

    feat:#增加列出文件pr

--- lib/mainwindow.cpp
+++ lib/mainwindow.cpp
@@ -13,7 +13,7 @@ MainWindow::MainWindow(QWidget *parent)
     lineEdit = new QLineEdit;
     lineEdit->setPlaceholderText("输入要删除的文件夹");
     lineEdit->setFont(QFont("PingFang SC", 10));
-    lineEdit->setFixedWidth(200);
+    lineEdit->setFixedWidth(240);
     lineEdit->setMinimumHeight(40);
     lineEdit->setProperty("class", "lineEdit");
     connect(lineEdit, SIGNAL(textChanged(QString)), this, SLOT(inpChange()));
@@ -27,26 +27,29 @@ MainWindow::MainWindow(QWidget *parent)
     connect(btn, SIGNAL(clicked()), this, SLOT(customClick()));
 
     inp_widget = new QWidget(this);
-    inp_widget->setMinimumWidth(w);
-    inp_widget->setMinimumHeight(60);
+    inp_widget->setFixedSize(w, 60);
     layout = new QHBoxLayout();
     layout->setSpacing(0);
     layout->setMargin(0);
     layout->addWidget(lineEdit);
     layout->addWidget(btn);
+    layout->setContentsMargins(0, 20, 0, 0);
     layout->setAlignment(Qt::AlignHCenter);
     inp_widget->setLayout(layout);
 
-    vlayout = new QVBoxLayout();
+
     bar_widget = new QWidget(this);
     bar_widget->setFixedWidth(w);
-    bar_widget->setFixedHeight(80);
+    bar_widget->setFixedHeight(50);
     progressBar = new QProgressBar;
     progressBar->setRange(0, 100);
-    progressBar->setFixedSize(280, 30);
+    progressBar->setFixedSize(330, 30);
     progressBar->setProperty("class", "processBar");
     progressBar->setVisible(false);
     layouts = new QHBoxLayout();
+    layouts->setAlignment(Qt::AlignBottom|Qt::AlignHCenter);
+    layouts->setMargin(0);
+    layouts->setSpacing(0);
     layouts->addWidget(progressBar);
     bar_widget->setLayout(layouts);
     timer = new QTimer();
@@ -55,21 +58,24 @@ MainWindow::MainWindow(QWidget *parent)
 
 
     list_widget = new QWidget(this);
-    list_widget->setFixedSize(w, 100);
+    list_widget->setFixedSize(w, 180);
     list_layout = new QHBoxLayout();
     list = new QListWidget(this);
-    list->setFixedSize(280, 60);
+    list->setFixedSize(330, 170);
     list->setObjectName("list");
-//    for (int i = 0; i < 10; i++) {
-//        item = new QListWidgetItem();
-//        item->setText("城市"+ QString::number(i));
-//        list->addItem(item);
-//    }
     list_layout->addWidget(list);
+    list_layout->setContentsMargins(0, 10, 0, 0);
+    list_layout->setAlignment(Qt::AlignHCenter);
     list_widget->setLayout(list_layout);
+    list_widget->setVisible(false);
+
+    vlayout = new QVBoxLayout();
+    vlayout->setSpacing(0);
+    vlayout->setMargin(0);
     vlayout->addWidget(inp_widget);
     vlayout->addWidget(list_widget);
     vlayout->addWidget(bar_widget);
+    vlayout->setAlignment(Qt::AlignTop);
     main_widget = new QWidget(this);
     main_widget->setFixedWidth(w);
     main_widget->setFixedHeight(300);
@@ -80,9 +86,7 @@ MainWindow::MainWindow(QWidget *parent)
     model->setAttribute(Qt::WA_DeleteOnClose);
     connect(model, SIGNAL(emitOn()), this, SLOT(handleOn()));
 
-    vlayout->setSpacing(0);
-    vlayout->setMargin(0);
-    vlayout->setAlignment(Qt::AlignHCenter);
+
     main_widget->setLayout(vlayout);
     this->setCentralWidget(main_widget);
 }
@@ -105,12 +109,17 @@ void MainWindow::customClick() {
         this->toast->showToast("文件路径或文件不存在");
         return;
     }
+    this->list_widget->show();
+    this->list->clear();
     QDir dir(str);
-    QStringList files = dir.entryList();
+    dir.setFilter (QDir::Dirs|QDir::Files);
+    QStringList filters;
+//    filters << "*.." << "*.";
+//    QStringList files = dir.entryList();
+    QList<QFileInfo>files = dir.entryInfoList(filters);
     for (int i = 0; i < files.count(); i++) {
-        qDebug() << files.at(i);
         item = new QListWidgetItem();
-        item->setText(files.at(i));
+        item->setText(files.at(i).fileName());
         this->list->addItem(item);
     }
     this->model->show();
@@ -126,6 +135,7 @@ void MainWindow::onTimeOut() {
         time = 0;
         this->lineEdit->setText("");
         str = "";
+        this->list->clear();
         this->progressBar->setVisible(false);
     }
 }
